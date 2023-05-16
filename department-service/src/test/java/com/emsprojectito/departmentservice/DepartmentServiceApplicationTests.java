4108ac6d390a6dbc881f8b36ff6a842b087d6a54
Lukito Andriansyah <lukitoandriansyah5@gmail.com>
2023-05-17 05:20:46 +0700

    initial comit

---
+++ department-service/src/test/java/com/emsprojectito/departmentservice/DepartmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emsprojectito.departmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
