64f7c0804e90b5cb78d06a7360801a5650508a9a
Lukito Andriansyah <lukitoandriansyah5@gmail.com>
2023-05-17 14:32:09 +0700

    [EMS-CONF-00001] Initialize

---
+++ config-server/src/test/java/com/emsprojectito/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emsprojectito.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
