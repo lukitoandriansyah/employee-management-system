91672afef4b5719aefc703072a82304b0f6a3e3b
Lukito Andriansyah <lukitoandriansyah5@gmail.com>
2023-05-24 12:41:09 +0700

    [EMS-EMOL-00003] Circuit Breaker Implementation

--- employee-service/src/main/java/com/emsprojectito/employeeservice/Service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/com/emsprojectito/employeeservice/Service/impl/EmployeeServiceImpl.java
@@ -8,8 +8,10 @@ import com.emsprojectito.employeeservice.dto.ApiResponseDto;
 import com.emsprojectito.employeeservice.dto.DepartmentDto;
 import com.emsprojectito.employeeservice.dto.EmployeeDto;
 import com.emsprojectito.employeeservice.mapper.EmployeeMapper;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 
 @Service
@@ -17,6 +19,7 @@ import org.springframework.stereotype.Service;
 public class EmployeeServiceImpl implements EmployeeService {
     private EmployeeRepository employeeRepository;
     private ApiClient apiClient;
+    private WebClient webClient;
     @Override
     public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
         Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
@@ -25,10 +28,33 @@ public class EmployeeServiceImpl implements EmployeeService {
         return dto;
     }
 
+    @CircuitBreaker(name = "${spring.application.name}", fallbackMethod = "getDefaultDepartment")
     @Override
     public ApiResponseDto getEmployeeById(Long id) {
         Employee employee = employeeRepository.findById(id).get();
-        DepartmentDto dto = apiClient.getDepartment(employee.getDepartmentCode());
+        /*DepartmentDto dto = apiClient.getDepartment(employee.getDepartmentCode());*/
+
+        DepartmentDto dto = webClient.get()
+                .uri("http://localhost:8080/api/departments/"+employee.getDepartmentCode())
+                .retrieve()
+                .bodyToMono(DepartmentDto.class)
+                .block();
+
+        EmployeeDto employeeDto = EmployeeMapper.mapToEmployeeDto(employee);
+        ApiResponseDto apiResponseDto = new ApiResponseDto();
+        apiResponseDto.setEmployee(employeeDto);
+        apiResponseDto.setDepartment(dto);
+        return apiResponseDto;
+    }
+
+    public ApiResponseDto getDefaultDepartment(Long employeeId, Exception e){
+        Employee employee = employeeRepository.findById(employeeId).get();
+
+        DepartmentDto dto = new DepartmentDto();
+        dto.setDepartmentName("R&D Department");
+        dto.setDepartmentCode("R&D 001");
+        dto.setDepartmentDescription("Research and Development Department");
+
         EmployeeDto employeeDto = EmployeeMapper.mapToEmployeeDto(employee);
         ApiResponseDto apiResponseDto = new ApiResponseDto();
         apiResponseDto.setEmployee(employeeDto);
